0fc5d056f6d2ecebbfd0ddf83e922bc54fce9813
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-17 14:14:59 +0200

    funcion escribir

--- Ejercicio 2/main.py
+++ Ejercicio 2/main.py
@@ -3,3 +3,9 @@ def pasar_mayus(frase1, frase2):
     frase2mayus = frase2.upper()
     return frase1mayus and frase2mayus
 
+def escribir(frase1, frase2):
+    archivo = open("/C:\Users\Usuario\Documents\GitHub\EstructurasDatos\Ejercicio 2/archivo.txt", "w")
+    archivo.write(frase1 + "\n")
+    archivo.write(frase2)
+    archivo.close()
+    return archivo
