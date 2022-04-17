ec5ed0dc1733beb5df1cd4693839fb5289b73ab7
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-17 15:39:22 +0200

    aa

--- Ejercicio 2/main.py
+++ Ejercicio 2/main.py
@@ -1,8 +1,11 @@
 import funciones
 
 if __name__ == "__main__":
-    funciones.vista.obtenerInput()
+    vista = funciones.vista("", "")
+    vista.obtenerInput()
+    print(vista.getFrase1())
 
-    frase_1_mayus = funciones.pasar_mayus(funciones.vista.getFrase1())
+
+    '''frase_1_mayus = funciones.pasar_mayus(funciones.vista.getFrase1())
     frase_2_mayus = funciones.pasar_mayus(funciones.vista.getFrase2())
-    funciones.escribir(frase_1_mayus, frase_2_mayus)
+    funciones.escribir(frase_1_mayus, frase_2_mayus)'''
