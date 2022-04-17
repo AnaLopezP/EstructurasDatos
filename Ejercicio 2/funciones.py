e441e8c895da50d179ed116b9aa0cdb33ffed660
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-17 14:20:59 +0200

    print en funcion

--- Ejercicio 2/funciones.py
+++ Ejercicio 2/funciones.py
@@ -1,6 +1,8 @@
 def pasar_mayus(frase1, frase2):
     frase1mayus = frase1.upper()
+    print(frase1mayus)
     frase2mayus = frase2.upper()
+    print(frase2mayus)
     return frase1mayus and frase2mayus
 
 def escribir(frase1, frase2):
---
+++ Ejercicio 2/main.py
@@ -0,0 +1,9 @@
+import funciones
+
+if __name__ == "__main__":
+    print("Introduzca la primera frase:")
+    frase_1 = str(input())
+    print("Introduzca la segunda frase:")
+    frase_2 = str(input())
+
+    funciones.pasar_mayus(frase_1, frase_2)
