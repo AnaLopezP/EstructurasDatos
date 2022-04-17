b3c9d8bbf9714c1a7631aa184aa62cad4546b50c
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-17 14:48:59 +0200

    añadir decimales

--- Ejercicio 3/main.py
+++ Ejercicio 3/main.py
@@ -2,6 +2,8 @@ import funciones
 
 if __name__ == "__main__":
     print("Introduzca el IVA elegido en porcentaje:")
-    iva_elegido = int(input())
+    print("Use un punto para indicar el decimal, si hay")
+    iva_elegido = float(input())
 
+    print("El precio final del producto, que sin IVA vale 100:")
     funciones.iva(iva_elegido)
